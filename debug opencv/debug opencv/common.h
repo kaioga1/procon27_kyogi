3be98cd08118ffb7e023bf4362b5ade2343dbe10
kobanoir <koba.kosen@gmail.com>
2016-09-21 13:18:00 +0900

    手動プログラムのプロジェクトの追加
    クラスでうまく実装できなかったのでとりあえず

---
+++ debug opencv/debug opencv/common.h
@@ -0,0 +1,26 @@
+#pragma once
+#pragma once
+
+//opencv
+# include "opencv2\opencv.hpp"
+# include <opencv2/core/core.hpp>
+# include <opencv2/highgui/highgui.hpp>
+# include <opencv2/imgproc/imgproc.hpp>
+# include <opencv/cvaux.h>
+
+//C++
+# include <iostream>
+# include <cmath>
+# include <algorithm>
+# include <list>
+# include <queue>
+# include <vector>
+# include <memory>
+
+/*
+‚Ç‚ê‚ªopencv‚ÌŠÖ”‚È‚Ì‚©‚í‚©‚ç‚È‚­‚È‚é‚Ì‚Å,
+cv‚Ì–¼‘O‹óŠÔ‚Í¡‰ñ‚Í‚È‚µ‚É‚µ‚Ü‚µ‚å‚¤
+*/
+//using namespace cv;
+using namespace std;
+;

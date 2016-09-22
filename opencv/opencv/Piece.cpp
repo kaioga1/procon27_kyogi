38446ed27de4da36cfe5cb7880c3148d01957958
sejimhp@gmail.com <sejimhp@gmail.com>
2016-09-22 20:46:14 +0900

    頂点検出既存のコード使ってみたけど微妙

--- opencv/opencv/Piece.cpp
+++ opencv/opencv/Piece.cpp
@@ -25,12 +25,53 @@ Piece::Piece(shared_ptr<cv::Mat> img, int num) {
 
 	//search_line();
 	//search_angle();
-	cout << endl;
 	//imshow(str, *image);
 	adr = cv::Point(10, 10);
 }
 
 void Piece::search_vertex() {
+	// (1)load a specified file as a 3-channel color image
+	cv::Mat harris_img = *image;
+
+	// (2)convert to a grayscale image and normalize it
+	cv::Mat gray_img = *image;
+	//cvtColor(eigen_img, gray_img, CV_BGR2GRAY);
+	normalize(gray_img, gray_img, 0, 255, cv::NORM_MINMAX);
+
+	// (3)detect and draw strong corners on the image based on Eigen Value
+	vector<cv::Point2f> corners;
+	goodFeaturesToTrack(gray_img, corners, 80, 0.01, 5);
+	vector<cv::Point2f>::iterator it_corner = corners.begin();
+	for (; it_corner != corners.end(); ++it_corner) {
+		//circle(eigen_img, cv::Point(it_corner->x, it_corner->y), 1, cv::Scalar(0, 200, 255), -1);
+		//circle(eigen_img, cv::Point(it_corner->x, it_corner->y), 8, cv::Scalar(0, 200, 255));
+	}
+
+	// (4)detect and draw strong corners on the image using Harris detector
+	goodFeaturesToTrack(gray_img, corners, 5, 0.01, 3, cv::Mat(), 3, true);
+	it_corner = corners.begin();
+	for (; it_corner != corners.end(); ++it_corner) {
+		circle(harris_img, cv::Point(it_corner->x, it_corner->y), 1, cv::Scalar(0, 255, 0), -1);
+		circle(harris_img, cv::Point(it_corner->x, it_corner->y), 8, cv::Scalar(0, 255, 0));
+	}
+
+	// (5)detect corners using high-speed corner detection; FAST
+	int threshold = 100;
+	bool nonmax = true;
+	vector<cv::KeyPoint> keypoints;
+	FAST(gray_img, keypoints, threshold, nonmax);
+	vector<cv::KeyPoint>::iterator it_kp = keypoints.begin();
+	for (; it_kp != keypoints.end(); ++it_kp) {
+		//circle(fast_img, cv::Point(it_kp->pt.x, it_kp->pt.y), 1, cv::Scalar(50, 0, 255), -1);
+		//circle(fast_img, cv::Point(it_kp->pt.x, it_kp->pt.y), 8, cv::Scalar(50, 0, 255));
+	}
+
+	// (6)show destination images, and quit when any key pressed
+	//cv::imshow("EigenValue", eigen_img);
+	cv::imshow("Harris", harris_img);
+	//cv::imshow("Fast", fast_img);
+	cv::waitKey(0);
+
 	//srcは入力画像をMatに変換したものを渡すこととする
 	/*vector<vector<cv::Point>> squares;
 	vector<vector<cv::Point> > contours;// 輪郭情報
@@ -39,19 +80,26 @@ void Piece::search_vertex() {
 											// 輪郭抽出
 	cv::findContours(*image, contours, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
 	*/
+	/*
 	cv::Mat src = *image;
 	cv::Mat dst = cv::Mat::zeros(src.rows, src.cols, CV_8UC3);
+	cv::Scalar color(rand() & 255, rand() & 255, rand() & 255);
 
 	vector<vector<cv::Point> > contours;
 	vector<cv::Vec4i> hierarchy;
 	findContours(src, contours, hierarchy,
-		CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
+		CV_RETR_CCOMP, CV_CHAIN_APPROX_TC89_KCOS);
+
+	//vector<cv::Point> p = contours[1];
+	//cv::approxPolyDP(p, approx, 0.1 , true);
+
+	/*for (int i = 0; i < approx.size(); i++) {
+		cout << approx[i] << " ";
+	}
 
 	for (int i = 0; i < contours.size(); i++) {
 		if (contours[i][0].x == 1 || contours[i][0].y == 1) { 
-			/*
-			外線がなぜが枠とされていたので, 省く
-			*/
+			//外線がなぜが枠とされていたので, 省く
 			auto itr = contours[i].begin();
 			while (itr != contours[i].end()) {
 				itr = contours[i].erase(itr);
@@ -60,7 +108,7 @@ void Piece::search_vertex() {
 		}
 		else {
 			//必要なのは始点と終点だけなのでその他の途中のデータは消去
-			auto itr = contours[i].begin();
+			/*auto itr = contours[i].begin();
 			while (itr + 1 != contours[i].end()) {
 				if (itr != contours[i].begin()) {
 					itr = contours[i].erase(itr);
@@ -73,121 +121,18 @@ void Piece::search_vertex() {
 		for (int j = 0; j < contours[i].size(); j++) {
 			cout << contours[i][j] << " ";
 		}
-		cout << endl;
 	}
+	cout << endl;
 	//cv::waitKey();
 
 	// トップレベルにあるすべての輪郭を横断し，
 	// 各連結成分をランダムな色で描きます．
 	int idx = 0;
 
-	cv::Scalar color(rand() & 255, rand() & 255, rand() & 255);
-	drawContours(dst, contours, idx, color, 1/*CV_FILLED*/, 8, hierarchy);
-	*image = dst;
+	drawContours(dst, contours, idx, color, 1, 8, hierarchy);
+	*image = dst;*/
 
-	/*for (size_t i = 0; i < contours.size(); i++)
-	{
-		cv::Mat contour = cv::Mat(contours[i]);
-
-		//凸包取得
-		vector<cv::Point> approx;
-		cv::convexHull(contour, approx);
-
-		// 輪郭・ポリライン近似 epsilonはarcLengthに対する割合で指定する
-		cv::approxPolyDP(approx, approx, 0.01 * cv::arcLength(contour, true), true);
-
-		//destは出力先のMat
-		cv::drawContours(*image, contours, i, CV_RGB(0, 0, 255), 4);
-		//ベクタに格納
-		squares.push_back(approx);
 	}
-	/*
-	//読み込んだ画像を元に頂点を求める
-	//描画用のキャンバスの作成
-	cv::Mat img = cv::Mat::zeros(500, 500, CV_8UC3);
-	cv::Mat im_canny;
-
-	cv::Point ten[50];
-
-	cv::Point zero = 0;
-
-	int a = 0, b = 0;
-	int apex = 0;
-	int standard = 0;
-	int count = 0;
-	int counter = 0;
-
-	Canny(*image, im_canny, 50, 200);
-
-	vector<cv::Vec4i> lines;
-
-	HoughLinesP(im_canny, lines, 1, CV_PI / 180, 85, 30, 10);
-
-	for (size_t i = 0; i < lines.size(); i++){
-		line(img, cv::Point(lines[i][0], lines[i][1]),
-			cv::Point(lines[i][2], lines[i][3]), cv::Scalar(0, 0, 255), 2, 8);
-	}
-
-	for (int i = 0; i < lines.size(); i++) {
-		for (int j = 0; j < 4; j++) {
-			if (j % 2 == 0) {
-				ten[a].x = lines[i][j];
-				a++;
-			}
-			else {
-				ten[b].y = lines[i][j];
-				b++;
-			}
-		}
-	}
-	for (apex = 0;; apex++) {
-		if (ten[apex] == zero) {
-			break;
-		}
-	}
-	cout << lines.size() << "," << apex << endl;
-	for (int i = 0; i < apex; i++) {
-		circle(img, cv::Point(ten[i].x, ten[i].y), 10, cv::Scalar(0, 255, 0), 1, 8);
-	}
-
-	for (int j = 0; j < 1600; j++) {
-		for (int x = 0; x < apex; x++) {
-			if (ten[standard].x + j >= ten[x].x && ten[standard].x - j <= ten[x].x && ten[standard].y + j >= ten[x].y && ten[standard].y - j <= ten[x].y && ten[standard] != ten[x]) {
-				if (j <= 5) {
-					vertex.push_back(make_shared<cv::Point>((ten[standard] + ten[x]) / 2));
-					cout << "!";
-					counter++;
-				}
-				cout << ten[standard] << ten[x] << endl;
-				if (x % 2 == 1) {
-					standard = x - 1;
-				}
-				else {
-					standard = x + 1;
-				}
-				count++;
-				j = 0;
-				break;
-			}
-		}
-		if (count == apex / 2) {
-			break;
-		}
-	}
-	number_of_corner = counter;
-	for (int i = 0; i < number_of_corner; i++) {
-		for (int j = 0; j < number_of_corner - i; j++) {
-			if (vertex[i] == vertex[j] && i != j) {
-				*vertex[j] = zero;
-				number_of_corner = j;
-			}
-		}
-	}
-	for (int i = 0; i < number_of_corner; i++) {
-		cout << *vertex[i] << endl;
-	}*/
-
-}
 
 void Piece::search_line() {
 	//頂点を元に辺を求める

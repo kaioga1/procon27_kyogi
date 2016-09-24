9644d92d7ba2a40e8d6d96c4baa455b8086e7985
sejimhp@gmail.com <sejimhp@gmail.com>
2016-09-24 18:50:35 +0900

    GUI)回転の追加

--- opencv/opencv/GUI.cpp
+++ opencv/opencv/GUI.cpp
@@ -1,10 +1,14 @@
 ﻿# include "GUI.h"
 
 // グローバル変数
-bool moving = false;
+bool lmoving = false;
 cv::Point mouse;
 vector<shared_ptr<Piece> > pieces;
 shared_ptr<Piece> piece = nullptr;
+//回転
+cv::Point click_point;	//クリックしたところの座標
+vector<cv::Point> src_point; //元の座標の配列
+bool rmoving = false;
 
 GUI::GUI() {
 	img = cv::Mat(cv::Size(960, 540), CV_8UC3, cv::Scalar(100, 100, 100));
@@ -16,15 +20,26 @@ void my_mouse_callback(int event, int x, int y, int flags, void* param) {
 
 	switch (event) {
 	case cv::EVENT_MOUSEMOVE:
-		if (moving) {
+		if (lmoving) {
 			//Mouseの位置ををピースに代入
 			piece->adr.x = x;
 			piece->adr.y = y;
 		}
+		else if (rmoving) {
+			//回転した値を代入
+			cv::Point C;
+			double rad = (click_point.y - y)/100.0;
+			vector<shared_ptr<cv::Point> > vertex = piece->get_vertex();
+			for (int i = 0; i < piece->get_number_of_corner(); i++) {
+				C.x = src_point[i].x * cos(rad) - src_point[i].y * sin(rad);
+				C.y = src_point[i].x * sin(rad) + src_point[i].y * cos(rad);
+				*vertex[i] = C;
+			}
+		}
 		break;
-
+	//左クリックでピースを移動
 	case cv::EVENT_LBUTTONDOWN:
-		moving = true;
+		lmoving = true;
 		for (int i = 0; i < pieces.size(); i++) {
 			//Pieceの画像データを取得
 			shared_ptr<cv::Mat> image = pieces[i]->image;
@@ -36,12 +51,43 @@ void my_mouse_callback(int event, int x, int y, int flags, void* param) {
 				break;
 			}
 			//なければfalse
-			if (i == pieces.size() - 1)	moving = false;
+			if (i == pieces.size() - 1) lmoving = false;
 		}
 		break;
 
 	case cv::EVENT_LBUTTONUP:
-		moving = false;
+		lmoving = false;
+		piece = nullptr;
+		break;
+	//右クリックで回転を行う
+	case cv::EVENT_RBUTTONDOWN:
+		//現在の座標を格納
+		click_point.x = x;
+		click_point.x = y;
+
+		rmoving = true;
+		for (int i = 0; i < pieces.size(); i++) {
+			//Pieceの画像データを取得
+			shared_ptr<cv::Mat> image = pieces[i]->image;
+			//Pieceの位置を取得
+			cv::Point adr = pieces[i]->adr;
+			//クリックした範囲内にあるか判定
+			if (x > adr.x && y > adr.y && x < adr.x + image->size().width && y < adr.y + image->size().height) {
+				vector<shared_ptr<cv::Point> > vertex = pieces[i]->get_vertex();
+				//変更前のデータを残しておく
+				for (int i = 0; i < vertex.size(); i++) {
+					src_point.push_back(*vertex[i]);
+				}
+				piece = pieces[i];
+				break;
+			}
+			//なければfalse
+			if (i == pieces.size() - 1) {rmoving = false;}
+		}
+		break;
+	case cv::EVENT_RBUTTONUP:
+		src_point.clear();
+		rmoving = false;
 		piece = nullptr;
 		break;
 	}
@@ -93,12 +139,10 @@ void GUI::draw(vector<shared_ptr<Piece> > pie, shared_ptr<Frame> frame) {
 				else {
 					line(temp, cv::Point(vertex[j]->x + p->adr.x, vertex[j]->y + p->adr.y), 
 						cv::Point(vertex[0]->x + p->adr.x, vertex[0]->y + p->adr.y), p->color, 1, 8);
+						cv::circle(temp, cv::Point(vertex[j]->x + p->adr.x, vertex[j]->y + p->adr.y), 5, cv::Scalar(0, 200, 0), 1, 8);
 				}
 	
 			}
-		}
-
-		if (moving) {
 		}
 
 		cv::imshow("GUI", temp);
@@ -107,7 +151,4 @@ void GUI::draw(vector<shared_ptr<Piece> > pie, shared_ptr<Frame> frame) {
 		if (cv::waitKey(15) == 27)
 			break;
 	}
-
-
-	//cv::imshow("GUI", img);
 }

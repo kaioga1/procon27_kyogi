c05491f1ed87a0453fc4e338e642cfb771fc092f
shimesaba0218 <shimesaba0218@gmail.com>
2016-10-06 22:04:38 +0900

    frameの切り取り

--- opencv/AnalysisGraphics.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-# include "Common.h"
-
-int AnalysisGraphics();
-void measureVertex(int &anscount, cv::Point ten[], cv::Point ans[], int line_end);
-void measureLine(int anscount, cv::Point ans[], double &root, double sen[]);
-void measureAngle(int anscount, cv::Point ans[], double naiseki[], double angle[], double naiseki_x[], double naiseki_y[], double sen[], double pie);
-void remakeFigure(int anscount, cv::Mat &img, cv::Point ans[]);
